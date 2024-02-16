03d6a1da8e27ff6b6c4b015ddf2262f8a573d3ef
Alena Savostyanova <el.savostyanova90@yandex.ru>
2024-02-17 00:49:51 +0300

    Hw 3. Annotation

--- seminar_3/double_list.py
+++ seminar_3/double_list.py
@@ -1,9 +1,9 @@
 """Дан список повторяющихся элементов lst. Вернуть список с
 дублирующимися элементами. В результирующем списке не должно быть
 дубликатов."""
-lst = [1, 1, 2, 2, 3, 3, 4, 5, 6, 5, 5, 5]
-res_list = []
-for i in set(lst):
-    if lst.count(i) > 1:
-        res_list.append(i)
+lst: list = [1, 1, 2, 2, 3, 3, 4, 5, 6, 5, 5, 5]
+res_list: list = []
+for item in set(lst):
+    if lst.count(item) > 1:
+        res_list.append(item)
 print(res_list)
